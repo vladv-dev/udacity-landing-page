29ba70d37d42f9fcb14ded18cd56c10f40cd93e6
Vladimir Vecera <vlad.v@protonmail.com>
2021-05-25 21:23:56 +0100

    Nav menu re-done using Javascript

--- js/app.js
+++ js/app.js
@@ -1,15 +1,39 @@
 // --- navBar --- //
 const navbar = document.querySelector("header");
 const navToggle = document.querySelector(".burger");
+const navContainer = document.querySelector(".navbar-container");
+const navMenu = ["About", "Features", "Pricing", "Team", "Contact"];
+const fragment = new DocumentFragment();
 
+// toggle the menu button
 navToggle.addEventListener("click", () => {
     navbar.classList.toggle("opened");
 });
-
+// creating items of the menu
+navMenu.forEach( function(item) {
+    const Ael = document.createElement("a");
+    Ael.textContent = item;
+    Ael.classList.add("navbar-link");
+    Ael.setAttribute("href", `#${item}`);
+    const navItem = document.createElement("li");
+    navItem.classList.add("navbar-item");
+    navItem.appendChild(Ael);
+    fragment.appendChild(navItem);
+});
+// creating the ul that contains menu items
+const navDiv = document.createElement("div");
+navDiv.classList.add("navbar-menu");
+const navUl = document.createElement("ul");
+navUl.classList.add("navbar-links");
+navUl.appendChild(fragment);
+navDiv.appendChild(navUl);
+navContainer.appendChild(navDiv);
 
 // --- highlighting current section of the page  +  scroll-up  button --- //
+
 const scrollUp = document.querySelector(".scroll-up-btn");
 const sections = document.querySelectorAll("section[id]");
+
 
 window.addEventListener("scroll", scrolling);
 function scrolling() {
@@ -20,11 +44,12 @@ function scrolling() {
     } else {
         scrollUp.classList.remove("show-scroll-up");
     }
-// -- defining sections in terms of height of element, relative to the parent, on the viewpoint
+// -- defining sections
     sections.forEach( section => {
         const sectionH = section.getBoundingClientRect().height;
         const sectionTop = section.offsetTop - 200;
         sectionID = section.getAttribute("id");
+        // highlighting current section in the top menu item list
         let currentSection = document.querySelector(".navbar-item a[href*=" + sectionID + "]");
         if (Yscroll > sectionTop && Yscroll <= sectionTop + sectionH) {
             currentSection.classList.add("current");
@@ -35,7 +60,7 @@ function scrolling() {
     
 }
 
-// --- best features tabs functionality ---- //
+// --- features tabs functionality ---- //
 
 const feature = document.querySelector(".features");
 const tabBtns = document.querySelectorAll(".tab-btn");
